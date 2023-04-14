43ab4aab15c480ec964afa4715e0778db2075222
Nischay <nischayverma2412@gmail.com>
2023-04-14 21:04:28 +0530

    Initial Commit

---
+++ DHT11-Weather/DHT11_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DHT11_WeatherApp.swift
+//  DHT11-Weather
+//
+//  Created by Nischay's Mac on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DHT11_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
